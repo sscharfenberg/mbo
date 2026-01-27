93b95c6c869b3267da5bd901f96a3b8ff278da3f
Sven Scharfenberg <5430451+sscharfenberg@users.noreply.github.com>
2026-01-27 18:12:40 +0100

    Registration scaffolding

---
+++ resources/app/components/Form/FormGroup.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+defineProps({
+    forId: String,
+    label: String
+});
+</script>
+
+<template>
+    <div class="form-group">
+        <label v-if="label?.length" :for="forId">{{ label }}</label>
+        <span v-else class="label" />
+        <div class="input"><slot /></div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use "Abstracts/mixins" as m;
+
+.form-group {
+    display: flex;
+    flex-flow: column wrap;
+
+    gap: 1ex;
+
+    @include m.mq("landscape") {
+        flex-flow: row wrap;
+
+        gap: 2ch;
+    }
+
+    label,
+    span.label {
+        flex-grow: 1;
+    }
+
+    .input {
+        flex: 0 0 100%;
+
+        @include m.mq("landscape") {
+            flex: 0 0 65%;
+            gap: 2ch;
+        }
+    }
+}
+</style>
--- resources/app/pages/Auth/Registration/RegistrationPage.vue
+++ resources/app/pages/Auth/Registration/RegistrationPage.vue
@@ -1,5 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import FormGroup from "Components/Form/FormGroup.vue";
+</script>
 
 <template>
-    <div>RegistrationPage</div>
+    <form method="POST" action="/auth/register">
+        <form-group for-id="name" label="Benutzername"><input type="text" name="name" id="name" /></form-group>
+        <form-group for-id="email" label="E-Mail-Adresse"><input type="email" name="email" id="email" /></form-group>
+        <form-group for-id="password" label="Passwort"
+            ><input type="password" name="password" id="password"
+        /></form-group>
+        <form-group><button type="submit" class="btn-primary">Submit!</button></form-group>
+    </form>
 </template>
