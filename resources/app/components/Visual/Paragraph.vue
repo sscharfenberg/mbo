22583b57c569a439587c7b9a2fd9a54ce2b43faf
Sven Scharfenberg <5430451+sscharfenberg@users.noreply.github.com>
2026-02-21 12:43:39 +0100

    Refactored Headline component so it accepts a size prop and can be use for different headline sizes. Added "Paragraph" visual component for <p> tags.

---
+++ resources/app/components/Visual/Paragraph.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+    <p><slot /></p>
+</template>
+
+<style scoped lang="scss">
+@use "sass:map";
+@use "Abstracts/colors" as c;
+@use "Abstracts/sizes" as s;
+
+p {
+    padding: map.get(s.$main, "paragraph", "padding");
+    border: map.get(s.$main, "paragraph", "border") solid map.get(c.$main, "paragraph", "border");
+
+    background-color: map.get(c.$main, "paragraph", "background");
+    color: map.get(c.$main, "paragraph", "surface");
+    border-radius: map.get(s.$main, "paragraph", "radius");
+}
+</style>
