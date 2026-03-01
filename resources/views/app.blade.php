6d6cf0c2c2c27898a6a57a104980969e7881b9a4
Sven Scharfenberg <5430451+sscharfenberg@users.noreply.github.com>
2026-03-01 09:09:38 +0100

    added favicons

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="color-scheme" content="light dark">
+
+        <link rel="icon" type="image/png" href="/favicons/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicons/favicon.svg" />
+        <link rel="shortcut icon" href="/favicons/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="MBO" />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+
         @if (file_exists(public_path('hot')))
             <meta name="hot" content="true" />
             @vite(['resources/app/main.ts'])
