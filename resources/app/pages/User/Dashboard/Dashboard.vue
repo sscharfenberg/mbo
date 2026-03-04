98014e4e1803c2773912777ad3705d13d320c6e8
Sven Scharfenberg <5430451+sscharfenberg@users.noreply.github.com>
2026-03-04 19:36:52 +0100

    Dashboard Nav styling and Intersection observer sticky position.

--- resources/app/pages/User/Dashboard/Dashboard.vue
+++ resources/app/pages/User/Dashboard/Dashboard.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { Head } from "@inertiajs/vue3";
+import DashboardNav from "@/pages/User/Dashboard/DashboardNav.vue";
 import DashboardPassword from "@/pages/User/Dashboard/DashboardPassword.vue";
 import DashboardProfile from "@/pages/User/Dashboard/DashboardProfile.vue";
 import TwoFactor from "@/pages/User/Dashboard/TwoFactor/TwoFactor.vue";
@@ -15,6 +16,7 @@ import Icon from "Components/UI/Icon.vue";
         <icon name="user-settings" :size="3" />
         {{ $t("pages.dashboard.title") }}
     </headline>
+    <dashboard-nav />
     <dashboard-password />
     <dashboard-profile />
     <two-factor />
